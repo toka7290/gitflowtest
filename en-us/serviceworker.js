f88d594971bbc1dbd45e49f94283c1e9e9cce0ae
toka7290 <toka7290@users.noreply.github.com>
2022-05-23 17:42:46 +0000

    deploy: 50e21c621a1f4a5f1698853aa7defffe0ed214e4

---
+++ en-us/serviceworker.js
@@ -0,0 +1,103 @@
+const CACHE_VERSION = "20220507";
+const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
+var urlsToCache = [
+  "./",
+  "./index.html",
+  "../css/color.min.css",
+  "../css/phone.min.css",
+  "../css/prism.min.css",
+  "../css/mc_format.min.css",
+  "../css/style.min.css",
+  "../img/close.svg",
+  "../img/error.svg",
+  "../img/github.svg",
+  "../img/help.svg",
+  "../img/homepage.svg",
+  "../img/icon.svg",
+  "../img/icon.webp",
+  "../img/icon_256.png",
+  "../img/icon_512.png",
+  "../img/icon_2000.png",
+  "../img/icon_apple-touch-icon.png",
+  "../img/import.svg",
+  "../img/maskable_icon.png",
+  "../img/more.svg",
+  "../img/share.svg",
+  "../img/subpacks.svg",
+  "../img/twitter.svg",
+  "../img/warning.svg",
+  "../js/main.min.js",
+  "../json/webapp.webmanifest",
+  "../lib/jquery-3.6.0.min.js",
+  "../lib/prism.js",
+  "../lib/mc_format.min.js",
+];
+
+// インストール処理
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // 指定されたファイルをキャッシュに追加する
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// アクティブ時
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => {
+          // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
+          return cacheName.startsWith(`${registration.scope}!`) && cacheName !== CACHE_NAME;
+        });
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheName) => {
+            // いらないキャッシュを削除する
+            return caches.delete(cacheName);
+          })
+        );
+      })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // キャッシュ内に該当レスポンスがあれば、それを返す
+      if (response) {
+        return response;
+      }
+
+      // 重要：リクエストを clone する。リクエストは Stream なので
+      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+      // 必要なので、リクエストは clone しないといけない
+      let fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then((requestResponse) => {
+        if (
+          !requestResponse ||
+          requestResponse.status !== 200 ||
+          requestResponse.type !== "basic"
+        ) {
+          // キャッシュする必要のないタイプのレスポンスならそのまま返す
+        } else {
+          // 重要：レスポンスを clone する。レスポンスは Stream で
+          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+          // 2つの Stream があるようにする
+          let responseToCache = requestResponse.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return requestResponse;
+      });
+    })
+  );
+});
